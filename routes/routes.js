ead1b721980d05ca28c609b3acee3f5d5c87ca61
Jorge Casal <casaldelacruz@gmail.com>
2020-07-16 15:28:15 +0200

    client created

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Model = require('../models/Model');
+const Model = require('../models/model');
 
 router.get('/', (req, res) => {
 	Model.find()
