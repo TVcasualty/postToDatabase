cf6db3f969dfeca13992536bb2e824f44342d8b6
Jorge Casal <casaldelacruz@gmail.com>
2020-07-20 11:16:30 +0200

    implementing update item

--- routes/routes.js
+++ routes/routes.js
@@ -15,10 +15,16 @@ router.post('/', (req, res) => {
 	newModel.save().then(item => res.json(item));
 });
 
+// router.update('/:id', (req, res) => {
+// 	Model.findById(req.params.id)
+// 		.then(item => item.update().then(() => res.json({ status: 'updated' })))
+// 		.catch(err => res.status(404).json({ status: 'no item found' }));
+// });
+
 router.delete('/:id', (req, res) => {
 	Model.findById(req.params.id)
 		.then(item => item.remove().then(() => res.json({ status: 'deleted' })))
-		.catch(err => res.status(404).json({ status: 'no quote found' }));
+		.catch(err => res.status(404).json({ status: 'no item found' }));
 });
 
 module.exports = router;
