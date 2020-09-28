53467f3f1f2ff5bf462fb393b604f73023ab5767
Jorge Casal <casaldelacruz@gmail.com>
2020-09-28 15:06:53 +0200

    user api post

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const User = require('../../models/User');
 
 router.post('/', (req, res) => {
-	Model.find()
-		res.send('register');
+    const { name, email, password} = req.body;
+    
+    if( !name || !email || !password ){
+        return res.status(400).json({msg: 'Please enter all info'})
+    };
+
+    User.findOne({ email })
+        .then(user => {
+            if(user) return res.status(400).json({msg: 'User already exists'});
+    
+            const newUser = new User ({
+                name,
+                email,
+                password
+        });
+    });
 });
+
 
 module.exports = router;
