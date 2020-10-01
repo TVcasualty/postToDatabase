3df57f7524e5f2f9df879ac5266a7f71693810bc
Jorge Casal <casaldelacruz@gmail.com>
2020-10-01 15:18:00 +0200

    error heade message

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -42,5 +42,11 @@ router.post('/', (req, res) => {
         })
 });
 
+router.get('/user', auth, (req, res) => {
+    User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+})
+
 
 module.exports = router;
