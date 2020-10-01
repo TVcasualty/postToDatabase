1fd6f4d0aee78a394223922bdd48c778016107f4
Jorge Casal <casaldelacruz@gmail.com>
2020-10-01 11:23:44 +0200

    headers test

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const auth = (req, res, next)=>{
     const token = req.header('x-auth-token');
 
-    if(!token) res.status(401).json({ msg: 'No authorization found'});
+    if(!token) res.status(401).json({ msg: 'Not authorized'});
 
     try{
         const decoded = jwt.verify(token, config.get('jwtSecret'));
